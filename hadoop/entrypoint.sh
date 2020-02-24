03ac807567dd0e4498f544f340da3546bcac8791
Zimo Xiao <xiaozimo@zuggr.com>
2020-02-24 17:44:19 -0600

    update

--- hadoop/entrypoint.sh
+++ hadoop/entrypoint.sh
@@ -23,56 +23,25 @@ export PATH="$PATH:/hadoop/sbin:/hadoop/bin"
 if [ $# -gt 0 ]; then
     exec "$@"
 else
-    for x in root hdfs yarn; do
-        if ! [ -f "$x/.ssh/id_rsa" ]; then
-            su - "$x" <<-EOF
-                [ -n "${DEBUG:-}" ] && set -x
-                ssh-keygen -t rsa -f ~/.ssh/id_rsa -N ""
-EOF
-        fi
-        if ! [ -f "$x/.ssh/authorized_keys" ]; then
-            su - "$x" <<-EOF
-                [ -n "${DEBUG:-}" ] && set -x
-                cp -v ~/.ssh/id_rsa.pub ~/.ssh/authorized_keys
-                chmod -v 0400 ~/.ssh/authorized_keys
-EOF
-        fi
-    done
-
-    if ! [ -f /etc/ssh/ssh_host_rsa_key ]; then
-        /usr/sbin/sshd-keygen || :
-    fi
-
     if ! pgrep -x sshd &>/dev/null; then
         /usr/sbin/sshd
     fi
     echo
     SECONDS=0
-    while true; do
-        if ssh-keyscan localhost 2>&1 | grep -q OpenSSH; then
-            echo "SSH is ready to rock"
-            break
-        fi
-        if [ "$SECONDS" -gt 20 ]; then
-            echo "FAILED: SSH failed to come up after 20 secs"
-            exit 1
-        fi
-        echo "waiting for SSH to come up"
-        sleep 1
-    done
+    # while true; do
+    #     if ssh-keyscan localhost 2>&1 | grep -q OpenSSH; then
+    #         echo "SSH is ready to rock"
+    #         break
+    #     fi
+    #     if [ "$SECONDS" -gt 20 ]; then
+    #         echo "FAILED: SSH failed to come up after 20 secs"
+    #         exit 1
+    #     fi
+    #     echo "waiting for SSH to come up"
+    #     sleep 1
+    # done
     echo
-    if ! [ -f /root/.ssh/known_hosts ]; then
-        ssh-keyscan localhost || :
-        ssh-keyscan 0.0.0.0   || :
-    fi | tee -a /root/.ssh/known_hosts
-    hostname="$(hostname -f)"
-    if ! grep -q "$hostname" /root/.ssh/known_hosts; then
-        ssh-keyscan "$hostname" || :
-    fi | tee -a /root/.ssh/known_hosts
-
     mkdir -pv /hadoop/logs
-
-    sed -i "s/localhost/$hostname/" /hadoop/etc/hadoop/core-site.xml
 
     start-dfs.sh
     start-yarn.sh
